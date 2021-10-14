65f8a950ae10407fa605b74014751d72bb530c99
ekowcharles <ekowcharles@yahoo.co.uk>
2021-10-13 20:42:30 -0500

    add la golang application

---
+++ log-aggregation/apps/golangapp/app.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+  "encoding/json"
+  "fmt"
+  "io"
+  "log"
+  "net/http"
+  "os"
+)
+
+type Payload struct {
+  Code string `json:"code"`
+  Description string `json:"desciption"`
+}
+
+type App struct {
+  Version string `json:"version"`
+}
+
+func main() {
+  defer fmt.Println("Exited!")
+
+  port := getEnv("PORT", "8993")
+  version := App{ getEnv("APP_VERSION", "0.0.0") }
+
+  http.HandleFunc("/", func(w http.ResponseWriter, rq *http.Request) {
+    JSON, _ := json.Marshal(version)
+
+    w.WriteHeader(http.StatusOK)
+    io.WriteString(w, string(JSON))
+  })
+
+  fmt.Printf("Listening on port %s ...\n", port)
+
+  log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func getEnv(key, fallback string) string {
+    value := os.Getenv(key)
+    if len(value) == 0 {
+        return fallback
+    }
+
+    return value
+}
