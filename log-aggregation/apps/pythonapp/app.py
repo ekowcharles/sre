fc1faa2fbc2efa8b87a57c6c1463084fbb0da69a
ekowcharles <ekowcharles@yahoo.co.uk>
2021-10-13 19:50:53 -0500

    refactor pythonapp codebase

--- log-aggregation/apps/pythonapp/app.py
+++ log-aggregation/apps/pythonapp/app.py
@@ -1,10 +1,9 @@
 import json
 import os
+import random
 from http import HTTPStatus
 
-from flask import Flask
-from flask import Response
-import random
+from flask import Flask, Response
 
 HTTP_STATUS_CODES = list(map(lambda x: x.value, HTTPStatus))
 HTTP_STATUS_CODES_COUNT = len(HTTP_STATUS_CODES)
@@ -14,32 +13,38 @@ APP_VERSION = os.environ.get('APP_VERSION')
 
 app = Flask(__name__)
 
-def create_bad_request_response():
-    status = HTTPStatus.BAD_REQUEST
 
-    data = json.dumps({
-                'status_code': status.value,
-                'description': status.phrase
-            })
+def create_response_payload(status):
+    """ Create response payload in app response format
+    """
+
+    return json.dumps({
+        'status_code': status.value,
+        'description': status.phrase
+    })
+
+def create_response(status):
+    """ Create response
+    """
 
     return Response(
-            data, 
-            status=status.value, 
+            create_response_payload(status),
+            status=status.value,
             mimetype='application/json'
         )
-    
 
 @app.route("/")
 def index():
     """ Return app version
     """
+
     data = json.dumps({
         'version': APP_VERSION
     })
 
     return Response(
         data, 
-        status=HTTPStatus.OK.value, 
+        status=HTTPStatus.OK.value,
         mimetype='application/json'
     )
 
@@ -48,44 +53,26 @@ def randomize():
     """ Return with a random HTTP response code. Include HTTP status code
         description in response
     """
-    index = random.randint(0, HTTP_STATUS_CODES_COUNT)
-    code = HTTP_STATUS_CODES[index]
-    status = list(filter(lambda x: x.value == code, HTTPStatus))[0]
-    data = json.dumps({
-            'status_code': code,
-            'description': status.phrase
-        })
 
-    return Response(
-        data, 
-        status=status.value, 
-        mimetype='application/json'
-    )
+    ind = random.randint(0, HTTP_STATUS_CODES_COUNT)
+    code = HTTP_STATUS_CODES[ind]
+    status = HTTPStatus(code)
+
+    return create_response(status)
 
 @app.route("/http/<code>")
 def http_code(code=HTTPStatus.OK.value):
     """ Return with provided HTTP response code. Return HTTP 400 - Bad request
         if code provided is invalid
     """
-    if not code.isnumeric():
-        return create_bad_request_response()
-    
+    status = HTTPStatus.BAD_REQUEST
+
     try:
         status = HTTPStatus(int(code))
-        data = json.dumps({
-                'status_code': status.value,
-                'description': status.phrase
-            })
-    except ValueError as e:
-        print(e)
+    except ValueError as error:
+        print(error)
 
-        return create_bad_request_response()
-    
-    return Response(
-            data, 
-            status=status.value, 
-            mimetype='application/json'
-        )
+    return create_response(status)
     
 
 @app.route("/exception")
